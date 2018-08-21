2875fdd39ff1799fced855ecc3876f3555b37029
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 01:52:17 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(collection) # put argument(s) here
   # code here
-  count = collection.size
-  while count > 0
+  i = 0
+  while i < array.length
   
-    count -= 1
+    i = i + 1
   end
 end
